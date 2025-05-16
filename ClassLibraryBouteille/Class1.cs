14fc9988d2eb8b1840e2bda87138722fcc7f227f
Caroline1609 <70326049+Caroline1609@users.noreply.github.com>
2025-05-16 10:22:05 +0200

    upload

---
+++ ClassLibraryBouteille/Class1.cs
@@ -0,0 +1,124 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace ClassLibraryBouteille
+{
+    public class Bouteille
+    {
+        private bool estOuvert;
+        private decimal contenuEnLitre;
+        private decimal contenanceEnLitre;
+
+        public bool EstOuvert { get => estOuvert; }
+        public decimal ContenuEnLitre { get => contenuEnLitre; }
+        public decimal ContenanceEnLitre { get => contenanceEnLitre; }
+
+        //Constructeur par défaut 
+        public Bouteille() : this(false, 0, 0) // utilisation de l'INTERDEPENDANCE pour éviter de faire du code redondant (on se base plutot sur le contructeur classique)
+
+        {
+
+        }
+
+        //Constructeur Par Clonage
+        public Bouteille(Bouteille bouteilleACloner) : this(bouteilleACloner.estOuvert, bouteilleACloner.contenanceEnLitre, bouteilleACloner.contenuEnLitre)
+
+        {
+
+        }
+
+        public Bouteille(bool _estOuvert, decimal contenuEnLitre, decimal contenanceEnLitre)
+        {
+
+            if (contenuEnLitre <= 0)
+            {
+                throw new ArgumentException("Le contenuEnLitre d'une bouteille ne peut être que positif", nameof(contenuEnLitre));
+            }
+
+
+            this.estOuvert = _estOuvert;
+            this.contenuEnLitre = contenuEnLitre;
+            this.contenanceEnLitre = contenanceEnLitre;
+        }
+        
+        public bool Ouvrir()
+        {
+            if (estOuvert == false)
+            {
+                estOuvert = true;
+                return true;                         
+            }
+            return false;
+
+        }
+
+        public bool Fermer()
+        {
+            if (estOuvert == true)
+            {
+                estOuvert = false;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public decimal GetContenuEnLitre()
+        {
+            return contenuEnLitre;
+        }
+
+        public decimal GetContenanceEnLitre()
+        {
+            return contenanceEnLitre;
+        }
+
+        public override string ToString()
+        {
+            return $"La bouteile est {(this.estOuvert ? "ouvertes" : "fermée")}. Elle a une contenance de {this.contenanceEnLitre} Litre(s) et elle est rempli de {this.contenuEnLitre} litres";
+        }
+
+        public decimal GetContenanceEnMLitre()
+        {
+            decimal resultat = contenuEnLitre * 1000;
+            return resultat;
+        }
+
+        public bool Remplir()
+        {
+            return Remplir(contenanceEnLitre - contenuEnLitre);
+        }
+
+        public bool Remplir(decimal quantiteEnLitre)
+        {
+            if (EstOuvert == true && (contenuEnLitre + quantiteEnLitre) <= contenanceEnLitre)
+            {
+                contenuEnLitre += quantiteEnLitre;
+                return true;
+            }
+            else
+            {
+                return false;
+            }            
+        }
+
+        public bool Vider()
+        {
+            return Vider(contenuEnLitre);
+        }
+
+        public bool Vider(decimal quantiteEnLitre)
+        {
+            if (this.estOuvert == true && quantiteEnLitre <= this.contenuEnLitre && contenuEnLitre != 0)
+            {
+                contenuEnLitre = contenuEnLitre - quantiteEnLitre;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ POOBouteille/Program.cs
@@ -0,0 +1,23 @@
+﻿using ClassLibraryBouteille;
+
+namespace POOBouteille
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Bouteille b1 = new Bouteille(true, 1.0m, 2.0m);
+
+            Console.WriteLine(b1.ToString());
+
+            b1.Ouvrir();
+            b1.Remplir(0.25m);
+
+
+            Console.WriteLine(b1.Ouvrir() ? "b1 est ouvert" : "b1 est fermer");
+            Console.WriteLine(b1.Remplir());
+
+
+        }
+    }
+}
